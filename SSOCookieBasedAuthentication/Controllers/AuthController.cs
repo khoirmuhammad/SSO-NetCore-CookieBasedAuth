5daabb48cd86d747bed7dc1a489a1a69086d4383
unknown <mk.muhammadkhoirudin@gmail.com>
2022-11-24 22:35:27 +0700

    Initial Project

---
+++ SSOCookieBasedAuthentication/Controllers/AuthController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace SSOCookieBasedAuthentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class AuthController : ControllerBase
+    {
+        private const string USERNAME = "demo";
+        private const string PASSWORD = "demo";
+
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login(string username, string password)
+        {
+            if (!USERNAME.Equals(username) || !PASSWORD.Equals(password))
+            {
+                return Unauthorized();
+            }
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, username),
+                new Claim("CustomClaim", "custom claim")
+            };
+
+            var claimsIdentity = new ClaimsIdentity(
+                claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            await HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity),
+                new AuthenticationProperties
+                {
+                    IsPersistent = false, // true : cookie will be deleted after expired. Even if the browser is closed, it will be exists. false : if browser is closed then cookie will be removed. It can be associated with remember me
+                    ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(1)
+                });
+
+            return Ok();
+        }
+
+
+        [HttpGet]
+        [Route("GetClaims")]
+        public IActionResult GetClaims()
+        {
+            string? username = HttpContext.User.Identity?.Name;
+            string? customClaim = HttpContext.User.FindFirst("CustomClaim")?.Value;
+
+            List<string?> result = new List<string?>();
+            result.Add(username);
+            result.Add(customClaim);
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("Logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return Ok();
+        }
+    }
+}
---
+++ SSOCookieBasedAuthentication2/Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.DataProtection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDataProtection()
+    .PersistKeysToFileSystem(new DirectoryInfo(@"D:\Me\Research\SSO\SSOCookieBasedAuthentication\CookieKey"))
+    .SetApplicationName("MyAppAuthCookie");
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        // Specify in case un-authenticated users
+        options.Events.OnRedirectToLogin = (context) =>
+        {
+            context.Response.StatusCode = 401;
+            //context.Properties.IsPersistent = true; //true : cookie will be deleted after expired. Even if the browser is closed, it will be exists. false : if browser is closed then cookie will be removed
+            return Task.CompletedTask;
+        };
+
+        // Specify the name of the auth cookie.
+        // ASP.NET picks a dumb name by default. "AspNetCore.Cookies"
+        options.Cookie.Name = "my_app_auth_cookie";
+    });
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
